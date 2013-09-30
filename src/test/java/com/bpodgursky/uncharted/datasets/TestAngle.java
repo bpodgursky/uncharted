d41e401ac15fe6208bc25e9846567e7b3f99ce46
Ben Podgursky <bpodgursky@liveramp.com>
2013-09-29 19:55:46 -0700

    bug

--- src/test/java/com/bpodgursky/uncharted/datasets/TestAngle.java
+++ src/test/java/com/bpodgursky/uncharted/datasets/TestAngle.java
@@ -10,6 +10,6 @@ public class TestAngle {
   @Test
   public void testConvert() throws Exception {
     Angle angle = new Angle(16, 33, 29.6);
-    assertEquals(0.28899549, angle.getRadians(), .00001);
+    assertEquals(4.3349324, angle.getRadians(), .00001);
   }
 }
