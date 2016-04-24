7d19998d0bd5ae6a355259fd2248641d0ba7be3d
Ben Podgursky <bpodgursky@liveramp.com>
2016-04-24 12:17:00 -0700

    ZOOM ZOOM

--- src/main/www/com/bpodgursky/uncharted/www/js/TweenControls.js
+++ src/main/www/com/bpodgursky/uncharted/www/js/TweenControls.js
@@ -1,20 +1,17 @@
 THREE.TweenControls = function (object) {
 
   this.camera = object;
-  this.targetX = null;
-  this.targetY = null;
-  this.targetZ = null;
 
-  this.startX = null;
-  this.startY = null;
-  this.startZ = null;
+  this.targetRotation = null;
+  this.startRotation = null;
+  this.midRotation = null;
+
+  this.targetPosition = null;
+  this.startPosition = null;
+  this.midPosition = null;
 
   this.tMax = null;
   this.tCurrent = null;
-
-  this.xMid = null;
-  this.yMid = null;
-  this.zMid = null;
 
   this.onComplete = null;
 
@@ -33,21 +30,46 @@ THREE.TweenControls = function (object) {
 
   };
 
-  this.lookAt = function (rotation, onComplete, time) {
+  this.lookAt = function (endRotation, endPosition, onComplete, time) {
 
-    this.startX = this.camera.rotation.x;
-    this.startY = this.camera.rotation.y;
-    this.startZ = this.camera.rotation.z;
+    this.startRotation = {
+      x: this.camera.rotation.x,
+      y: this.camera.rotation.y,
+      z: this.camera.rotation.z
+    };
 
-    this.targetX = this.getTarget(this.startX, rotation.x);
-    this.targetY = this.getTarget(this.startY, rotation.y);
-    this.targetZ = this.getTarget(this.startZ, rotation.z);
+    this.targetRotation = {
+      x: this.getTarget(this.startRotation.x, endRotation.x),
+      y: this.getTarget(this.startRotation.y, endRotation.y),
+      z: this.getTarget(this.startRotation.z, endRotation.z)
+    };
+
+    this.midRotation = {
+      x: (this.camera.rotation.x + this.targetRotation.x) / 2,
+      y: (this.camera.rotation.y + this.targetRotation.y) / 2,
+      z: (this.camera.rotation.z + this.targetRotation.z) / 2
+    };
+
+    this.startPosition = {
+      x: this.camera.position.x,
+      y: this.camera.position.y,
+      z: this.camera.position.z
+    };
+
+    this.targetPosition = {
+      x: endPosition.x,
+      y: endPosition.y,
+      z: endPosition.z
+    };
+
+    this.midPosition = {
+      x: (this.startPosition.x + this.targetPosition.x) / 2,
+      y: (this.startPosition.y + this.targetPosition.y) / 2,
+      z: (this.startPosition.z + this.targetPosition.z) / 2
+    };
 
     this.tMax = time;
     this.tCurrent = 0;
-    this.xMid = (this.camera.rotation.x + this.targetX) / 2;
-    this.yMid = (this.camera.rotation.y + this.targetY) / 2;
-    this.zMid = (this.camera.rotation.z + this.targetZ) / 2;
 
     this.onComplete = onComplete;
   };
@@ -56,11 +78,16 @@ THREE.TweenControls = function (object) {
     return mid + (target - start) / 2 * Math.sin(Math.PI * this.tCurrent / this.tMax - Math.PI / 2)
   };
 
+  this.updateVec = function (mid, start, target, update) {
+    update.x = this.updateVar(mid.x, target.x, start.x);
+    update.y = this.updateVar(mid.y, target.y, start.y);
+    update.z = this.updateVar(mid.z, target.z, start.z);
+  };
+
   this.update = function (delta) {
 
-    this.camera.rotation.x = this.updateVar(this.xMid, this.targetX, this.startX);
-    this.camera.rotation.y = this.updateVar(this.yMid, this.targetY, this.startY);
-    this.camera.rotation.z = this.updateVar(this.zMid, this.targetZ, this.startZ);
+    this.updateVec(this.midRotation, this.startRotation, this.targetRotation, this.camera.rotation);
+    this.updateVec(this.midPosition, this.startPosition, this.targetPosition, this.camera.position);
 
     this.tCurrent += delta;
 
