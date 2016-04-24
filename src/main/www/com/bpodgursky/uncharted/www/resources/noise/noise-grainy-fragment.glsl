8ffaf60b5f08334f042eea45bff9a242e9bc3b13
Ben Podgursky <bpodgursky@liveramp.com>
2016-04-23 22:02:13 -0700

    cleanup

--- src/main/www/com/bpodgursky/uncharted/www/resources/noise/noise-grainy-fragment.glsl
+++ src/main/www/com/bpodgursky/uncharted/www/resources/noise/noise-grainy-fragment.glsl
@@ -6,8 +6,6 @@
 
 			uniform float time;
 
-//			uniform vec4 colors[1024];
-
 			vec4 permute( vec4 x ) {
 
 				return mod( ( ( x * 34.0 ) + 1.0 ) * x, 289.0 );
@@ -132,78 +130,62 @@ const int octaves = 4;
 
       float temp = (highTemp * (total)  +(1.0-total) * lowTemp);
 
-      //     var indexHigh = Math.floor((star.temperatureEstimate - 800.0) * spectrumData.width / 29200.0) * 4+8;
-      float i =(temp - 800.0)*1024.0/29200.0;
+      //  these equations reproduce the RGB values of this image: https://www.seedofandromeda.com/assets/images/blogs/star_spectrum_3.png
 
+      float i =(temp - 800.0)*0.035068;
 
       //  for R
-      bool rbucket1 = (i) < 60.0;   //  0, 255 in 60
-      bool rbucket2 = (i) >= 60.0 && (i) < 236.0;  //   255,255
-      bool rbucket3 = (i) >= 236.0 && (i) < 288.0; //  255,128
-      bool rbucket4 = (i) >= 288.0 && (i) < 377.0; //  128,60
-      bool rbucket5 = (i) >= 377.0 && (i) < 511.0; //  60,0
-      bool rbucket6 = (i) >= 511.0;  //  0,0
+      bool rbucket1 = i < 60.0;   //  0, 255 in 60
+      bool rbucket2 = i >= 60.0 && i < 236.0;  //   255,255
+      bool rbucket3 = i >= 236.0 && i < 288.0; //  255,128
+      bool rbucket4 = i >= 288.0 && i < 377.0; //  128,60
+      bool rbucket5 = i >= 377.0 && i < 511.0; //  60,0
+      bool rbucket6 = i >= 511.0;  //  0,0
 
-      bool gbucket1 = float(i) <60.0;
-      bool gbucket2 = float(i) >= 60.0 && float(i) < 103.0; //  0,100
-      bool gbucket3 = float(i) >= 103.0 && float(i) < 133.0; // 100,233
-      bool gbucket4 = float(i) >= 133.0 && float(i) < 174.0; // 233, 255
-      bool gbucket5 = float(i) >= 174.0 && float(i) < 236.0; // 255,255
-      bool gbucket6 = float(i) >= 236.0 && float(i) < 286.0; //255,193
-      bool gbucket7 = float(i) >= 286.0 && float(i) < 367.0; //193,129
-      bool gbucket8 = float(i) >= 367.0 && float(i) < 511.0; //129,64
-      bool gbucket9 = float(i) >= 511.0; // 64,32
+      bool gbucket1 = i <60.0;
+      bool gbucket2 = i >= 60.0 && i < 103.0; //  0,100
+      bool gbucket3 = i >= 103.0 && i < 133.0; // 100,233
+      bool gbucket4 = i >= 133.0 && i < 174.0; // 233, 255
+      bool gbucket5 = i >= 174.0 && i < 236.0; // 255,255
+      bool gbucket6 = i >= 236.0 && i < 286.0; //255,193
+      bool gbucket7 = i >= 286.0 && i < 367.0; //193,129
+      bool gbucket8 = i >= 367.0 && i < 511.0; //129,64
+      bool gbucket9 = i >= 511.0; // 64,32
 
      // for B
-      bool bbucket1 = float(i) < 103.0;
-      bool bbucket2 = float(i) >= 103.0 && float(i) < 133.0; // 0,211
-      bool bbucket3 = float(i) >= 133.0 && float(i) < 173.0; // 211,247
-      bool bbucket4 = float(i) >= 173.0 && float(i) < 231.0;  //  247,255
-      bool bbucket5 = float(i )>= 231.0;
-
-//      i = 0.0;
+      bool bbucket1 = i < 103.0;
+      bool bbucket2 = i >= 103.0 && i < 133.0; // 0,211
+      bool bbucket3 = i >= 133.0 && i < 173.0; // 211,247
+      bool bbucket4 = i >= 173.0 && i < 231.0;  //  247,255
+      bool bbucket5 = i>= 231.0;
 
       float r =
-        float(rbucket1) * (0.0 + (i) * (255.0-0.0)/60.0) +
+        float(rbucket1) * (0.0 + i * 4.25) +
         float(rbucket2) * (255.0) +
-        float(rbucket3) * (255.0 + (i - 236.0) * (128.0 - 255.0)/(288.0-236.0)) +
-        float(rbucket4) * (128.0 + (i - 288.0) * (60.0 - 128.0)/(377.0 - 288.0)) +
-        float(rbucket5) * (60.0 + (i - 377.0) * (0.0 - 60.0)/(511.0-377.0))+
+        float(rbucket3) * (255.0 + (i - 236.0) * -2.442) +
+        float(rbucket4) * (128.0 + (i - 288.0) * -0.764) +
+        float(rbucket5) * (60.0 + (i - 377.0) * -0.4477)+
         float(rbucket6) * 0.0;
 
       float g =
          float(gbucket1) * (0.0) +
-         float(gbucket2) * (0.0 + (i - 60.0) *(100.0-0.0)/(103.0-60.0)) +
-         float(gbucket3) * (100.0 + (i - 103.0) *(233.0- 100.0)/(133.0-103.0))+
-         float(gbucket4) * (233.0 + (i - 133.0) *(255.0 - 233.0)/(174.0 - 133.0))+
+         float(gbucket2) * (0.0 + (i - 60.0) *2.3255) +
+         float(gbucket3) * (100.0 + (i - 103.0) *4.433)+
+         float(gbucket4) * (233.0 + (i - 133.0) *0.53658)+
          float(gbucket5) * (255.0) +
-         float(gbucket6) * (255.0 +(i - 236.0) *(193.0 - 255.0)/(286.0 - 236.0)) +
-         float(gbucket7) * (193.0 + (i - 286.0) * (129.0 - 193.0)/(367.0 - 286.0)) +
-         float(gbucket8) * (129.0 + (i - 367.0) * (64.0 - 129.0)/(511.0 - 367.0))+
-         float(gbucket9) * (64.0 + (i - 511.0) * (32.0-64.0)/(1024.0 - 511.0));
+         float(gbucket6) * (255.0 +(i - 236.0) *-1.24) +
+         float(gbucket7) * (193.0 + (i - 286.0) *-0.7901) +
+         float(gbucket8) * (129.0 + (i - 367.0) * -0.45138)+
+         float(gbucket9) * (64.0 + (i - 511.0) * -0.06237);
 
       float b =
         float(bbucket1) * 0.0+
-        float(bbucket2) * (0.0 + (i - 103.0) * (211.0 - 0.0)/(133.0-103.0)) +
-        float(bbucket3) * (211.0 + (i - 133.0) * (247.0-211.0)/(173.0-133.0))+
-        float(bbucket4) * (247.0 + (i - 173.0)*(255.0-247.0)/(231.0-173.0))+
+        float(bbucket2) * (0.0 + (i - 103.0) * 7.0333) +
+        float(bbucket3) * (211.0 + (i - 133.0) * 0.9)+
+        float(bbucket4) * (247.0 + (i - 173.0)*0.1379)+
         float(bbucket5) * 255.0;
 
       gl_FragColor = vec4(vec3(r/255.0, g/255.0, b/255.0), 1.0);
-
-
-
-//      if(t1 > .3){
-//gl_FragColor = vec4(vec3(rHigh, gHigh, bHigh), aHigh);
-//
-//}else{
-//gl_FragColor = vec4(vec3(rLow, gLow, bLow), aLow);
-//}
-
-//vec3 minus = vec3(total, total, total);
-//        vec3 normColor = normalize(vec3(r, g, b)) - minus;
-//
-//        gl_FragColor = vec4(normColor, 255.0);
 
 
 			}
